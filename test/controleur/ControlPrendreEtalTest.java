507f140a7289116d832e6f0efdfd37a716775495
mathisrouzaud <115712581+mathisrouzaud@users.noreply.github.com>
2026-04-02 17:42:54 +0200

    test

---
+++ test/controleur/ControlPrendreEtalTest.java
@@ -0,0 +1,39 @@
+package controleur;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import villagegaulois.Village;
+
+class ControlPrendreEtalTest {
+	private Village village;
+	private ControlVerifierIdentite controlVerifierIdentite;
+	
+	@BeforeEach
+	void setUp(){
+		
+	}
+
+	@Test
+	void testControlPrendreEtal() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testResteEtals() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testVerifierIdentite() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testPrendreEtal() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ test/controleur/ControlVerifierIdentiteTest.java
@@ -0,0 +1,37 @@
+package controleur;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import personnages.Chef;
+import personnages.Gaulois;
+import villagegaulois.Village;
+
+class ControlVerifierIdentiteTest {
+	private Village village;
+	private Chef chef;
+	@BeforeEach
+	void setUp() {
+		System.out.println("Initialisation...");
+		village = new Village("le vllage des irréductibles", 10, 5);
+		Chef chef = new Chef("Asterix", 5, village);
+		village.setChef(chef);
+		
+	}
+
+	@Test
+	void testControlVerifierIdentite() {
+		ControlVerifierIdentite controlVerifierIdentite = new ControlVerifierIdentite(village);
+		assertNotNull(controlVerifierIdentite, "Constructeur ne renvoie pas null");
+	}
+
+	@Test
+	void testVerifierIdentite() {
+		ControlVerifierIdentite controlVerifierIdentite = new ControlVerifierIdentite(village);
+		assertTrue(controlVerifierIdentite.verifierIdentite("Asterix"));		
+	}
+
+}
